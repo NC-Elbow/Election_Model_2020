fd11679301c6020b62f281ba2cc9028e3c2f5a42
Clark Alexander <gcalexander1981@gmail.com>
2020-06-23 23:51:22 -0500

    Add files via upload

--- election2020.py
+++ election2020.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import plot, hist
+from numpy.random import choice, randint
 
 electoral_dict = {}
 states = ['AL','AK','AZ','AR','CA','CO','CT','DC','DE','FL','GA','HI','ID','IL','IN',
@@ -26,8 +27,46 @@ probabilities = [[.5,.5 ]]*51
 for k in range(len(states)):
     electoral_dict[states[k]] = (votes[k], probabilities[k])
     
+
+def compute_probabilities(dictionary):
+    #Here we need to do most of the work explaining how we get the likelihood
+    # of each state's probability of voting one way or the other
+    for key in dictionary.keys():
+        r_pts = randint(1,10)# this is where the work is
+        d_pts = randint(1,11)
+        total_pts = r_pts+d_pts
+        dictionary[key][1][0] = d_pts/total_pts
+        dictionary[key][1][1] = r_pts/total_pts
+        del r_pts, d_pts
+    return dictionary
+
     
     
+def simulate_election(dictionary):
+    D = 0
+    R = 0
+    for key in dictionary.keys():
+        elected = choice(['D','R'], p = dictionary[key][1])
+        if elected == 'D':
+            D += dictionary[key][0]
+        else:
+            R += dictionary[key][0]
+    return D,R        
     
+    
+def many_votes(dictionary, num_votes):
+     num_D_wins = 0
+     num_R_wins = 0
+     draws = 0
+     for k in range(num_votes):
+         D,R = simulate_election(compute_probabilities(dictionary))
+         if D > R:
+             num_D_wins += 1
+         elif R > D:
+             num_R_wins += 1
+         else:
+             draws += 1    
+     return num_D_wins, num_R_wins, draws
+        
     
     
